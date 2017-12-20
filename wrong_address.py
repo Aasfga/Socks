489d0aa4cc9b3dfb23cd50aca6a7e2083bd36f43
Dominik Grybos <dominik.grybos@gmail.com>
2017-12-20 14:11:23 +0100

    README and many domains

--- wrong_address.py
+++ wrong_address.py
@@ -2,6 +2,6 @@ from socks import Socks, print_all
 
 
 socks = Socks(('localhost', 4567), 'dominik')
-connection = socks.create_connection(("probably_wrong_address.pl", 80))
+connection = socks.create_connection(("probably_wrong_address.com", 80))
 connection.send(b"wrong protocol")
 print_all(connection)
